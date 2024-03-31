b77ce272a7b7754806687505bb6fca7eccc8bc6d
Karl Perkins <karl@karlperkins.me>
2024-04-01 11:41:56 +1300

    add search history

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const API_KEY = 'fb3d9117fe114f65a0920703243003';
 const API_URL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}`;
 
+const weatherList = [];
+
 async function getWeatherByLocation(location) {
   const response = await fetch(`${API_URL}&q=${location}`);
   if (response.ok) {
@@ -21,30 +23,62 @@ function parseResults(results) {
   };
 }
 
-let weather = {};
+function renderWeather() {
+  const weatherListElement = document.querySelector('#weather-list');
+  weatherListElement.innerHTML = '';
+
+  weatherList.forEach((weather) => {
+    const weatherItem = document.createElement('div');
+    weatherItem.classList.add('weather');
+  
+    const location = document.createElement('div');
+    location.classList.add('location')
+    location.textContent = `${weather.location}, ${weather.country}`;
+  
+    const icon = document.createElement('img');
+    icon.classList.add('icon')
+    icon.src = weather.conditionIcon;
+  
+    const condition = document.createElement('div');
+    condition.classList.add('condition')
+    condition.textContent = weather.conditionText;
+  
+    const temperature = document.createElement('div');
+    temperature.classList.add('temperature')
+    temperature.textContent = `${weather.celsius} \u00B0C`;
+  
+    weatherItem.appendChild(location);
+    weatherItem.appendChild(icon);
+    weatherItem.appendChild(condition);
+    weatherItem.appendChild(temperature);
+  
+    weatherListElement.appendChild(weatherItem);
+  });
+}
+
 document.querySelector('form').addEventListener('submit', async (event) => {
   event.preventDefault();
   const formData = new FormData(event.target);
   const location = formData.get('location');
-  weather = await getWeatherByLocation(location).then((results) =>
+  
+  const weather = await getWeatherByLocation(location).then((results) =>
     parseResults(results),
   );
 
-  document.querySelector('.condition').textContent = weather.conditionText;
-  document.querySelector('.location').textContent =
-    `${weather.location}, ${weather.country}`;
-  document.querySelector('.icon').src = weather.conditionIcon;
-  document.querySelector('.temp').textContent = weather.celsius;
+  weatherList.push(weather);
+
+  renderWeather();
 });
 
-const btn = document.querySelector('.toggle');
-const temp = document.querySelector('.temp');
-btn.addEventListener('click', () => {
-  if (btn.textContent === '\u00B0C') {
-    btn.textContent = '\u00B0F';
-    temp.textContent = weather.fahrenheit;
-  } else {
-    btn.textContent = '\u00B0C';
-    temp.textContent = weather.celsius;
-  }
-});
+
+// const btn = document.querySelector('.toggle');
+// const temp = document.querySelector('.temp');
+// btn.addEventListener('click', () => {
+//   if (btn.textContent === '\u00B0C') {
+//     btn.textContent = '\u00B0F';
+//     temp.textContent = weather.fahrenheit;
+//   } else {
+//     btn.textContent = '\u00B0C';
+//     temp.textContent = weather.celsius;
+//   }
+// });
